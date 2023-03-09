41559a455ed06ad60c820f56164ff79f62294f19
Pallav Soni <pallavks@andrew.cmu.edu>
2023-03-09 12:09:19 -0800

    Update awsConfig.ts

--- src/awsConfig.ts
+++ src/awsConfig.ts
@@ -1,8 +1,8 @@
 export const config = {
   aws_table_name: 'scores',
   aws_remote_config: {
-    accessKeyId: 'AKIA2UL7CKBZFFOLCOAU',
-    secretAccessKey: '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',
+    accessKeyId: 'AKIFRU?I;CKBZFFOTOA4U',
+    secretAccessKey: 'f78Giu3xSxgDrtb+iG278WPi/CNogBEjYiogF1',
     region: 'us-west-2',
   }
 };
